37adaa6ad2220a22a029cb0e8a9432d2ab315c54
KISHORESAMALA1725 <kishore.9686@gmail.com>
2025-04-07 07:44:27 +0530

    Update dockerPipeline.groovy

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,9 +1,7 @@
 import com.i27academy.builds.Docker;
 
 def call(Map pipelineParams) {
-    Docker docker = new Docker(this)
-
-    // Define the pipeline
+    Docker docker = new Docker(this) 
     pipeline {
         agent {
             label "k8s-slave"
@@ -30,7 +28,7 @@ def call(Map pipelineParams) {
             TEST_HOST_PORT = "${pipelineParams.testHostPort}"
             STAGE_HOST_PORT = "${pipelineParams.stageHostPort}"
             PROD_HOST_PORT = "${pipelineParams.prodHostPort}"
-            CONT_PORT = "${pipelineParams.contPort}"
+            CONT_PORT = "${env.CONT_PORT}"
             POM_VERSION = readMavenPom().getVersion()
             POM_PACKAGING = readMavenPom().getPackaging()
             DOCKER_HUB = 'docker.io/kishoresamala84'
@@ -46,7 +44,7 @@ def call(Map pipelineParams) {
                 }
                 steps {
                     script {
-                        buildApp("${env.APPLICATION_NAME}")
+                        docker.buildApp("${env.APPLICATION_NAME}")
                     }
                 }
             }
@@ -70,53 +68,126 @@ def call(Map pipelineParams) {
                 }
             }
 
-            // More stages like 'Build Format', 'Docker Build Push', etc.
-            // The rest of the stages can remain the same.
-        }
-    }
-
-}
-
-// Define helper methods outside the pipeline block
-def buildApp(String applicationName) {
-    echo "***** Building the Application *****"
-    sh "mvn clean package -DskipTest=true"
-    archiveArtifacts 'target/*.jar'
-}
-
-def dockerBuildPush() {
-    echo "****** Building Docker image *******"
-    sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd"
-    sh "docker build --no-cache --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
-    withCredentials([usernamePassword(credentialsId: 'kishoresamala84_docker_creds', usernameVariable: 'DOCKER_CREDS_USR', passwordVariable: 'DOCKER_CREDS_PSW')]) {
-        sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
-    }
-    sh "docker push ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
-}
-
-def imageValidation() {
-    echo '***** Attempting to pull the Docker image *****'
-    try {
-        sh "docker pull ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
-        echo '***** Docker Image Pulled successfully *****'
-    } catch (Exception e) {
-        echo "OOOPS!!! Docker Image with this tag not found, so building the image now..."
-        buildApp(env.APPLICATION_NAME)
-        dockerBuildPush()
-    }
-}
-
-def deployToDocker(String envDeploy, String hostPort, String contPort) {
-    echo "***** Deploying to $envDeploy environment *****"
-    withCredentials([usernamePassword(credentialsId: 'john_docker_vm_passwd', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        script {
-            try {
-                sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker stop ${env.APPLICATION_NAME}-'$envDeploy'\""
-                sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker rm ${env.APPLICATION_NAME}-'$envDeploy'\""
-            } catch (err) {
-                echo "Error Caught: $err"
+            stage('***** BUILD FORMAT *****') {
+                steps {
+                    script {
+                        sh "echo SOURCE JAR file i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING}"
+                        sh "echo TARGET JAR file i27-${env.APPLICATION_NAME}-${currentBuild.number}-${BRANCH_NAME}.${env.POM_PACKAGING}"
+                    }
+                }
             }
-            sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker container run -dit -p $hostPort:$contPort --name ${env.APPLICATION_NAME}-'$envDeploy' ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}\""
+
+            stage('***** Docker-Build-Push *****') {
+                when {
+                    expression {
+                        params.buildOnly == 'yes' && params.scanOnly == 'yes' && params.dockerbuildandpush == 'yes'
+                    }
+                }
+                steps {
+                    script {
+                        dockerBuildPush()
+                    }
+                }
+            }
+
+            stage('***** Deploy to DEV-ENV *****') {
+                when {
+                    expression {
+                        params.deployto'dev' == 'yes'
+                    }
+                }
+                steps {
+                    script {
+                        deployToDocker('dev', "${env.DEV_HOST_PORT}", "${env.CONT_PORT}")
+                    }
+                }
+            }
+
+            stage('***** Deploy to TEST-ENV *****') {
+                when {
+                    expression {
+                        params.deploytotest == 'yes'
+                    }
+                }
+                steps {
+                    script {
+                        deployToDocker('test', "${evn.TEST_HOST_PORT}", "${env.CONT_PORT}")
+                    }
+                }
+            }
+
+            stage('***** Deploy to STAGE-ENV *****') {
+                when {
+                    expression {
+                        params.deploytostage == 'yes'
+                    }
+                }
+                steps {
+                    script {
+                        imageValidation()
+                        deployToDocker('stage', "${env.STAGE_HOST_PORT}", "${env.CONT_PORT}")
+                    }
+                }
+            }
+
+            stage('***** Deploy to PROD-ENV *****') {
+                when {
+                    expression {
+                        params.deploytoprod == 'yes'
+                    }
+                }
+                steps {
+                    script {
+                        deployToDocker('prod', "${env.PROD_HOST_PORT}", "${env.CONT_PORT}")
+                    }
+                }
+            }
         }
     }
+
+    def buildApp() {
+        echo "***** Building the Application *****"
+        sh "mvn clean package -DskipTest=true"
+        archiveArtifacts 'target/*.jar'
+    }
+
+    def dockerBuildPush() {
+        echo "****** Building Docker image *******"
+        sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd"
+        sh "docker build --no-cache --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
+        withCredentials([usernamePassword(credentialsId: 'kishoresamala84_docker_creds', usernameVariable: 'DOCKER_CREDS_USR', passwordVariable: 'DOCKER_CREDS_PSW')]) {
+            sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
+        }
+        sh "docker push ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+    }
+
+    def imageValidation() {
+        echo '***** Attempting to pull the Docker image *****'
+        try {
+            sh "docker pull ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+            echo '***** Docker Image Pulled successfully *****'
+        } catch (Exception e) {
+            echo "OOOPS!!! Docker Image with this tag not found, so building the image now..."
+            buildApp()
+            dockerBuildPush()
+        }
+    }
+
+    def deployToDocker(envDeploy, hostPort, contPort) {
+        echo "***** Deploying to $envDeploy environment *****"
+        withCredentials([usernamePassword(credentialsId: 'john_docker_vm_passwd', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+            script {
+                try {
+                    sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker stop ${env.APPLICATION_NAME}-'$envDeploy'\""
+                    sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker rm ${env.APPLICATION_NAME}-'$envDeploy'\""
+                } catch (err) {
+                    echo "Error Caught: $err"
+                }
+                sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker container run -dit -p $hostPort:$contPort --name ${env.APPLICATION_NAME}-'$envDeploy' ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}\""
+            }
+        }
+    }
+
 }
+
+
