cee478eefe2ebfba7c28a0aac65232a56c940548
KISHORESAMALA1725 <kishore.9686@gmail.com>
2025-04-07 08:47:15 +0530

    Update dockerPipeline.groovy

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,50 +1,7 @@
 import com.i27academy.builds.Docker
 
 // Move all method definitions outside the pipeline block
-def buildApp() {
-    echo "***** Building the Application *****"
-    sh "mvn clean package -DskipTest=true"
-    archiveArtifacts 'target/*.jar'
-}
 
-def dockerBuildPush() {
-    echo "****** Building Docker image *******"
-    sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd"
-    sh "docker build --no-cache --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
-    withCredentials([usernamePassword(credentialsId: 'kishoresamala84_docker_creds', usernameVariable: 'DOCKER_CREDS_USR', passwordVariable: 'DOCKER_CREDS_PSW')]) {
-        sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
-    }
-    sh "docker push ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
-}
-
-def imageValidation() {
-    echo '***** Attempting to pull the Docker image *****'
-    try {
-        sh "docker pull ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
-        echo '***** Docker Image Pulled successfully *****'
-    } catch (Exception e) {
-        echo "OOOPS!!! Docker Image with this tag not found, so building the image now..."
-        buildApp()
-        dockerBuildPush()
-    }
-}
-
-def deployToDocker(envDeploy, hostPort, contPort) {
-    echo "***** Deploying to $envDeploy environment *****"
-    withCredentials([usernamePassword(credentialsId: 'john_docker_vm_passwd', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        script {
-            // Add dev_ip as part of the parameters
-            def dev_ip = pipelineParams.dev_ip  // Make sure dev_ip is passed as a parameter
-            try {
-                sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker stop ${env.APPLICATION_NAME}-'$envDeploy'\""
-                sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker rm ${env.APPLICATION_NAME}-'$envDeploy'\""
-            } catch (err) {
-                echo "Error Caught: $err"
-            }
-            sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker container run -dit -p $hostPort:$contPort --name ${env.APPLICATION_NAME}-'$envDeploy' ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}\""
-        }
-    }
-}
 
 // The pipeline block
 def call(Map pipelineParams) {
@@ -191,4 +148,49 @@ def call(Map pipelineParams) {
             }
         }
     }
+
+    def buildApp() {
+    echo "***** Building the Application *****"
+    sh "mvn clean package -DskipTest=true"
+    archiveArtifacts 'target/*.jar'
+}
+
+def dockerBuildPush() {
+    echo "****** Building Docker image *******"
+    sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd"
+    sh "docker build --no-cache --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
+    withCredentials([usernamePassword(credentialsId: 'kishoresamala84_docker_creds', usernameVariable: 'DOCKER_CREDS_USR', passwordVariable: 'DOCKER_CREDS_PSW')]) {
+        sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
+    }
+    sh "docker push ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+}
+
+def imageValidation() {
+    echo '***** Attempting to pull the Docker image *****'
+    try {
+        sh "docker pull ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+        echo '***** Docker Image Pulled successfully *****'
+    } catch (Exception e) {
+        echo "OOOPS!!! Docker Image with this tag not found, so building the image now..."
+        buildApp()
+        dockerBuildPush()
+    }
+}
+
+def deployToDocker(envDeploy, hostPort, contPort) {
+    echo "***** Deploying to $envDeploy environment *****"
+    withCredentials([usernamePassword(credentialsId: 'john_docker_vm_passwd', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        script {
+            // Add dev_ip as part of the parameters
+            def dev_ip = pipelineParams.dev_ip  // Make sure dev_ip is passed as a parameter
+            try {
+                sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker stop ${env.APPLICATION_NAME}-'$envDeploy'\""
+                sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker rm ${env.APPLICATION_NAME}-'$envDeploy'\""
+            } catch (err) {
+                echo "Error Caught: $err"
+            }
+            sh "sshpass -p '$PASSWORD' ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker container run -dit -p $hostPort:$contPort --name ${env.APPLICATION_NAME}-'$envDeploy' ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}\""
+        }
+    }
+}
 }
